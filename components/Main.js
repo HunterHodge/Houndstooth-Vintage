53988230f8d2e67379454fe3facaa98b1a8f762d
Hunter <hunterdhodge@gmail.com>
2019-08-26 14:46:33 -0500

    Fix error in linking

--- components/Main.js
+++ components/Main.js
@@ -2,8 +2,8 @@ function stockBuilder(stockItems){
     return stockItems.map((item) => {
         return `
         <div class='stockItem'>
-            <a href="./stock/${item.Id}data-navigo${item.Id}>
-                <div><img src='http://images.pixieset.com/68114502/${item.image}-large.jpg></div>
+            <a href="./stock/${item.id}data-navigo${item.id}">
+                <figure><img src='http://images.pixieset.com/68114502/${item.id}-large.jpg'></figure>
                 <div>
                     <h3>${item.name}</h3>
                     <h4>${item.price}</h4>
