63dc0b1cceab2d145bef253d367c140846add76f
Hunter <hunterdhodge@gmail.com>
2019-08-26 15:03:28 -0500

    fix store

--- index.js
+++ index.js
@@ -8,28 +8,6 @@ import Navigo from "navigo";
 const router = new Navigo(location.origin);
 
 const store = {
-<<<<<<< HEAD
-    'home': {
-        'links': {
-            'primary': [ 'Home' ]
-        },
-        'title': 'Home',
-        'store': [
-            {
-                'id': '6f40965ae02e5f4d59389cedcfeec08c',
-                'name': 'Karen Scott',
-                'price': '$30.00',
-                'description': 'lorem ipsum dolor sit amet consectiur adpecing elit'
-            },
-            {
-                'id': '601159ddcd58ee6bf2ebdf61ed997e6f',
-                'name': 'Jacket',
-                'price': '$50.00',
-                'description': 'filler text'
-            }
-        ]
-    }
-=======
   home: {
     links: {
       primary: ["Home"]
@@ -50,7 +28,6 @@ const store = {
       }
     ]
   }
->>>>>>> 0a9b6f5c0fa16dff3285d4233bc02c143fa9a7ad
 };
 
 function render(state) {
