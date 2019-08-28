b74d258886bdaf7a844a698850e096caed857c34
Hunter <hunterdhodge@gmail.com>
2019-08-28 11:27:44 -0500

    Add session storage to Index.js

--- index.js
+++ index.js
@@ -7,55 +7,81 @@ import Navigo from "navigo";
 
 const router = new Navigo(location.origin);
 
+const sessionStorage = {
+  products: [
+    {
+      id: "6f",
+      name: "Plaid shirt",
+      price: "$30.00",
+      description: "A plaid shirt with inner lining by Karen Scott",
+      imgs: "6f40965ae02e5f4d59389cedcfeec08c-large.jpg"
+    },
+    {
+      id: "60",
+      name: "Jacket",
+      price: "$50.00",
+      description: "A heavy green jacket with a swirly pattern",
+      imgs: "601159ddcd58ee6bf2ebdf61ed997e6f-large.jpg"
+    },
+    {
+      id: "d0",
+      name: "Sweater",
+      price: "$30.00",
+      description:
+        "A red sleeveless sweater with a logo on the right side of the chest",
+      imgs: "d0ab37fc6366613390d25f55529b7f60-large.jpg"
+    }
+  ],
+  cart: [{}]
+};
+
 const store = {
   home: {
     links: {
-      primary: ["Home"]
+      primary: ["Home", "Cart"]
     },
-    title: "Home",
-    products: [
-      {
-        id: "6f",
-        name: "Plaid shirt",
-        price: "$30.00",
-        description: "A plaid shirt with inner lining by Karen Scott",
-        imgs: "6f40965ae02e5f4d59389cedcfeec08c-large.jpg"
-      },
-      {
-        id: "60",
-        name: "Jacket",
-        price: "$50.00",
-        description: "A heavy green jacket with a swirly pattern",
-        imgs: "601159ddcd58ee6bf2ebdf61ed997e6f-large.jpg"
-      },
-      {
-        id: "d0",
-        name: "Sweater",
-        price: "$30.00",
-        description:
-          "A red sleeveless sweater with a logo on the right side of the chest",
-        imgs: "d0ab37fc6366613390d25f55529b7f60-large.jpg"
-      }
-    ]
+    title: "Home"
+  },
+  cart: {
+    links: {
+      primary: ["Home", "Cart"]
+    },
+    title: "Cart"
   }
 };
 
-function render(state) {
+function render(state, data) {
   document.querySelector("#root").innerHTML = `
     ${Navigation(state)}
     ${Header(state)}
-    ${Main(state)}
+    ${Main(state, data)}
     ${Footer(state)}
     `;
 
   router.updatePageLinks();
+
+  if (document.querySelector(".stockItem")) {
+    let stockItems = document.querySelectorAll(".stockItem");
+    stockItems.forEach(
+      addEventListener("click", e => {
+        sessionStorage.cart.push(
+          e.target
+            .closest("a")
+            .getAttributeNode("href")
+            .textContent.slice(-2)
+        );
+      })
+    );
+  }
+
+  console.log(sessionStorage.cart);
 }
 
 router
   .on(":view", params => {
-    render(store[params.view]);
+    render(store[params.view], sessionStorage);
   })
   .on("/", () => {
-    render(store.home);
+    render(store.home, sessionStorage);
   })
   .resolve();
