baab31ba26d234e64a3b32a4f3bcd18d53aeafc4
Hunter <hunterdhodge@gmail.com>
2019-08-28 20:02:58 -0500

    Remove no longer needed div from header

--- components/Header.js
+++ components/Header.js
@@ -2,12 +2,5 @@ export default state => `
 
 <header>
 <div id='headerLogo'><h1>Houndstooth Vintage</h1></div>
-<div>
-    <ul>
-        <li>cart</li>
-        <li>login</li>
-        <li>search</li>
-    </ul>
-</div>
 </header>
 `;
